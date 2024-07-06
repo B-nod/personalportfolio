82d61e75f51c29958e0d63f7d229f49ca89b0491
B-nod <binodtamang245@gmail.com>
2024-07-06 17:22:00 +0545

    second

--- folio/admin.py
+++ folio/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models import *
+
 
 # Register your models here.
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('format_date',)
+
+    def format_date(self, obj):
+        return obj.data.strftime('%b, %Y')
+
+    format_date.admin_order_field = 'date'
+    format_date.short_description = 'Date'
+admin.site.register(Prot)
+admin.site.register(Skill)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Client)
+admin.site.register(Project)
