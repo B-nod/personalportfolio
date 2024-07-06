87b4901e16eb92ccef414fbc2396225c19e2c874
B-nod <binodtamang245@gmail.com>
2024-07-06 12:05:34 +0545

    next

--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import *
 # Create your views here.
+
+def homepage(request):
+    prot = Prot.objects.all()
+    skill = Skill.objects.all()
+    education = Education.objects.all()
+    experience = Experience.objects.all()
+    client = Client.objects.all()
+    project = Project.objects.all()
+
+    context = {
+        'prot': prot,
+        'skill':skill,
+        'education':education,
+        'experience': experience,
+        'client':client,
+        'project':project
+    }
+    return render(request, 'folio/index.html', context)
